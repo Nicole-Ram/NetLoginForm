3532438fa6400ced1d4b1c48daf74012549c256f
Nicole-Ram <54059496+Nicole-Ram@users.noreply.github.com>
2019-08-18 14:40:18 +0200

    Add files via upload

---
+++ NettechApplicationWeb/build/generated-sources/ap-source-output/io/nettech/entities/Client_.java
@@ -0,0 +1,17 @@
+package io.nettech.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-07-08T16:02:24")
+@StaticMetamodel(Client.class)
+public class Client_ { 
+
+    public static volatile SingularAttribute<Client, Double> customernumber;
+    public static volatile SingularAttribute<Client, String> website;
+    public static volatile SingularAttribute<Client, String> surname;
+    public static volatile SingularAttribute<Client, String> name;
+    public static volatile SingularAttribute<Client, Long> id;
+
+}
